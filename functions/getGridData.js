6620806f09938a11d56e338e63999ffd36d4eb0e
Philip Eschenbacher <philip.eschenbacher@mongodb.com>
2021-10-29 10:15:42 +0200

    test

--- functions/getGridData.js
+++ functions/getGridData.js
@@ -2,7 +2,6 @@ exports = async ({ startRow, endRow, sortModel, groups, groupKeys }) => {
   const cluster = context.services.get("mongodb-atlas");
   const collection = cluster.db("GridDemo").collection("OlympicWinners");
   const query = {}
-  // This is a meaningful comment
   const sort = sortModel.length <= 0 ? {_id:1} : context.functions.execute('translateSortModel', sortModel);
   const lastRow = await collection.count(query);
   const rows = await collection.find(query).sort(sort).skip(startRow).limit(endRow-startRow).toArray();
